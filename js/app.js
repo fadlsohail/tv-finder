c08770b08000004d8b227636a9002914143832e4
Fadl Sohail <mfadlsohail@gmail.com>
2026-06-01 01:34:28 +0100

    read the search input

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const searchForm = document.querySelector("#search-form");
+const keywordInput = document.querySelector("#keywords");
 const statusMessage = document.querySelector("#status-message");
 
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    statusMessage.textContent = "Search clicked";
+    const keyword = keywordInput.value;
+
+    statusMessage.textContent = "you searched for: " + keyword;
 });
