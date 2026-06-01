47b4bfd1045c06fdca4f5f585de3c63ec9e999fb
Fadl Sohail <mfadlsohail@gmail.com>
2026-06-01 02:01:00 +0100

    show images in cards

--- js/app.js
+++ js/app.js
@@ -19,8 +19,15 @@ searchForm.addEventListener("submit", function (event) {
             data.forEach(function (item) {
                 const show = item.show;
 
+                let image = "";
+
+                if (show.image) {
+                    image = show.image.medium;
+                }
+
                 const showCard = `
                 <div class="bg-neutral-800 rounded border border-neutral-700 p-4">
+                     <img class="w-full rounded mb-4 show-poster" src="${image}" alt="${show.name} poster">
                     <h3 class="text-xl font-semibold mb-2">${show.name}</h3>
                 </div>
             `;
